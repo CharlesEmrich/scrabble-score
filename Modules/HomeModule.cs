81b3e4193e95ae80a1f6ce5f77a1881a91bb1c99
Adrian Agnic and Charles Emrich <adrian+charles@>
2017-06-01 09:49:24 -0700

    add Views

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,3 +1,19 @@
-// using ScrabbleScore.Objects;
-//
-// namespace ScrabbleScore;
+using Nancy;
+using ScrabbleScore.Objects;
+
+namespace ScrabbleScore
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        return View["index.cshtml"];
+      };
+      Post["/"] = _ => {
+        ScrabbleWord newWord = new ScrabbleWord(Request.Form["word"]);
+        return View["index.cshtml", newWord];
+      };
+    }
+  }
+}
